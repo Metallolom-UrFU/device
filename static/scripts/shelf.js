de562c43357ce3e33f135bed25370871607d32fb
Mark <180091136+VisCube@users.noreply.github.com>
2026-01-17 06:35:35 +0500

    Update shelf.js

--- static/scripts/shelf.js
+++ static/scripts/shelf.js
@@ -2,7 +2,7 @@ async function fetchDoor() {
     let response = await fetch("/fetch-door");
     let data = await response.json();
     let door = data["door"];
-    if (door) window.location.replace("/index");
+    if (door) window.location.replace("/");
 }
 
 setInterval(fetchDoor, 1000);
